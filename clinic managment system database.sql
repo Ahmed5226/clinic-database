5931888fccc53398436e51b1cf1a582baff2cf63
Ahmed mohamed <66845200+Ahmed5226@users.noreply.github.com>
2020-06-13 02:19:29 +0200

    Update clinic managment system database.sql

--- clinic managment system database.sql
+++ clinic managment system database.sql
@@ -43,7 +43,7 @@ CREATE TABLE `clinic`.`bill` (
 
 
 
-SELECT * FROM clinic.patient;
+
 
 INSERT INTO `clinic`.`patient` (`patientID`, `First_name`, `last_name`, `type`, `age`, `address`) VALUES ('1', 'ahmed', 'mohamed', 'male', '20', 'alex');
 INSERT INTO `clinic`.`patient` (`patientID`, `First_name`, `last_name`, `type`, `age`, `address`) VALUES ('2', 'ali', 'omar', 'male', '15', 'cairo');
@@ -51,13 +51,13 @@ INSERT INTO `clinic`.`patient` (`patientID`, `First_name`, `last_name`, `type`, 
 INSERT INTO `clinic`.`patient` (`patientID`, `First_name`, `last_name`, `type`, `age`, `address`) VALUES ('4', 'nour', 'mahmoud', 'male', '19', 'cairo');
 INSERT INTO `clinic`.`patient` (`patientID`, `First_name`, `last_name`, `type`, `age`, `address`) VALUES ('5', 'basma', 'maher', 'fmale', '30', 'bhera');
 
+SELECT * FROM clinic.patient;
 
 
 
 
 
 
-SELECT * FROM clinic.checkup;
 
 INSERT INTO `clinic`.`checkup` (`checkID`, `patientId`, `findings`, `complainsl`, `treatId`, `medicineId`, `NumberOfItem`, `date`) VALUES ('1', '1', 'id300', 'breath', '5', '500', '4', '2019-5-1');
 INSERT INTO `clinic`.`checkup` (`checkID`, `patientId`, `findings`, `complainsl`, `treatId`, `medicineId`, `NumberOfItem`, `date`) VALUES ('2', '2', 'id200', 'breath', '6', '300', '5', '2020-5-1');
@@ -65,13 +65,13 @@ INSERT INTO `clinic`.`checkup` (`checkID`, `patientId`, `findings`, `complainsl`
 INSERT INTO `clinic`.`checkup` (`checkID`, `patientId`, `findings`, `complainsl`, `treatId`, `medicineId`, `NumberOfItem`, `date`) VALUES ('4', '4', 'id900', 'breath', '4', '200', '6', '2018-5-1');
 INSERT INTO `clinic`.`checkup` (`checkID`, `patientId`, `findings`, `complainsl`, `treatId`, `medicineId`, `NumberOfItem`, `date`) VALUES ('5', '5', 'id170', 'breath', '6', '400', '3', '2015-1-4');
 
+SELECT * FROM clinic.checkup;
 
 
 
 
 
 
-SELECT * FROM clinic.bill;
 
 INSERT INTO `clinic`.`bill` (`BillID`, `total`, `patientID`) VALUES ('1', '200', '1');
 INSERT INTO `clinic`.`bill` (`BillID`, `total`, `patientID`) VALUES ('2', '450', '2');
@@ -79,11 +79,11 @@ INSERT INTO `clinic`.`bill` (`BillID`, `total`, `patientID`) VALUES ('3', '600',
 INSERT INTO `clinic`.`bill` (`BillID`, `total`, `patientID`) VALUES ('4', '150', '4');
 INSERT INTO `clinic`.`bill` (`BillID`, `total`, `patientID`) VALUES ('5', '800', '5');
 
+SELECT * FROM clinic.bill;
 
 
 
 
-SELECT * FROM clinic.medicines;
 
 INSERT INTO `clinic`.`medicines` (`medicinID`, `medicineName`, `Number`, `Detailing`, `available`, `expiry`) VALUES ('1', 'panadol', '4', 'help us', '19', '2050-1-4');
 INSERT INTO `clinic`.`medicines` (`medicinID`, `medicineName`, `Number`, `Detailing`, `available`, `expiry`) VALUES ('2', 'aspren', '5', 'help', '80', '2020-1-5');
@@ -91,11 +91,10 @@ INSERT INTO `clinic`.`medicines` (`medicinID`, `medicineName`, `Number`, `Detail
 INSERT INTO `clinic`.`medicines` (`medicinID`, `medicineName`, `Number`, `Detailing`, `available`, `expiry`) VALUES ('4', 'catavast', '6', 'help', '70', '2040-5-9');
 INSERT INTO `clinic`.`medicines` (`medicinID`, `medicineName`, `Number`, `Detailing`, `available`, `expiry`) VALUES ('5', 'omberazol', '5', 'help', '60', '2040-5-4');
 
+SELECT * FROM clinic.medicines;
 
 
 
-
-SELECT * FROM clinic.treatments;
 
 INSERT INTO `clinic`.`treatments` (`treatmentID`, `treatType`) VALUES ('1', 'cannot breath');
 INSERT INTO `clinic`.`treatments` (`treatmentID`, `treatType`) VALUES ('2', 'hurt');
@@ -103,3 +102,4 @@ INSERT INTO `clinic`.`treatments` (`treatmentID`, `treatType`) VALUES ('3', 'leg
 INSERT INTO `clinic`.`treatments` (`treatmentID`, `treatType`) VALUES ('4', 'heart');
 INSERT INTO `clinic`.`treatments` (`treatmentID`, `treatType`) VALUES ('5', 'arm');
 
+SELECT * FROM clinic.treatments;
